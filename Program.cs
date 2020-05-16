2eea3c20c88be6265e34ba9f3eab4c177f671754
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-05-15 21:18:37 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Digite qualquer coisa... veja o que acontece!!!!");
+            string frase1 = Console.ReadLine();
+            string frase2 = frase1.Replace("r","l").Replace("R","L");
+
+            Console.WriteLine(frase2);   
+        }
+    }
+}
